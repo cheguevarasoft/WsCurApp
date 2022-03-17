079fddcfdb21324758d31c214b8ef3190aee7472
Ehtiram <ehtiram.hajikhanov@gmail.com>
2022-03-17 16:55:33 +0400

    salam

---
+++ WsCurApp/WsCur.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WsCur.asmx.cs" Class="WsCurApp.WsCur" %>
---
+++ WsCurApp/WsCur.asmx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace WsCurApp
+{
+    /// <summary>
+    /// Summary description for WsCur
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WsCur : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        //CURRENCY - MEZENNE
+        [WebMethod]
+        public XmlDocument GetCurrency(string curname)
+        {
+            string result = @"<root>{result}</root>";//response model
+
+            XmlDocument xmldoc = new XmlDocument();
+
+           
+            string date = DateTime.Now.ToString("dd.MM.yyyy");
+
+            xmldoc.Load("https://www.cbar.az/currencies/" + date + ".xml?wsdl");
+
+            
+            XmlNodeList xmlist = xmldoc.GetElementsByTagName("Valute");
+
+
+            foreach (XmlNode item in xmlist)
+            {
+                
+                if (item.Attributes[0].Value == curname)
+                {
+                    result = result.Replace("{result}", item.InnerXml);
+                }
+            }
+
+            XmlDocument myxml = new XmlDocument();
+
+            myxml.LoadXml(result);
+            
+            return myxml;
+        }
+
+
+    }
+}
